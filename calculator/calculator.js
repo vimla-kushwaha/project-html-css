28ca19350d053d598d6d11b413d29457b2e5632e
vimla-kushwaha <vimlakushwaha01@gmail.com>
2024-03-29 10:08:34 +0530

    caculator using div and form tag

---
+++ calculator/calculator.js
@@ -0,0 +1,5 @@
+// next part is completion of javascript part for calcultor design
+// step-1:- We need to add an hoover effect when we reach an button and and call on-mouse click event and fetch value by clicking on button
+//step1.1:- for doing that we will require to fetch id of element and apply on-mouse hover effect on that 
+const val = document.getElementById("b");
+document.write(val);
