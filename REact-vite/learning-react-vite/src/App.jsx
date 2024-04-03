fca4d9d048da7685d192bf59ab1e63d20258d114
vimla-kushwaha <vimlakushwaha01@gmail.com>
2024-04-03 22:42:21 +0530

    commit

--- REact-vite/learning-react-vite/src/App.jsx
+++ REact-vite/learning-react-vite/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+//import React from 'react'
+import{Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline text-red-500">
-    hello world!
-  </h1>
+    <main className="bg-slate-300/20">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={'Home'} />
+            <Route path="/about" element={'About'} />
+            <Route path="/projects" element={'Projects'} />
+            <Route path="/contact" element={'Contact'} />
+          </Routes>
+        </Router>
+    </main>
   )
 }
 
